b4ec7987fa1857343531fe3669757753a32c4fd7
Minerkow <melnikov.ignat@phystech.edu>
2021-03-03 02:16:34 +0200

    reorganisation

--- AVL_Tree/Error.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-enum AvlError_t {AVLERR_OK = 0,
-                 AVLERR_NOT_INIT,
-                 AVLERR_NULL_POINTER_ARG,
-                 AVLERR = -1,
-                 AVLERR_INSERT,
-                 AVLERR_ITERATOR
-                };
